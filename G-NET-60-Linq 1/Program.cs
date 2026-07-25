2b05f97efe795bf55bbd7893f41ddc418e1aca6e
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-26 01:59:32 +0300

    Question01

--- G-NET-60-Linq 1/Program.cs
+++ G-NET-60-Linq 1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace G_NET_60_Linq_1
+﻿
+using G_NET_60_Linq_1;
+using static G_NET_60_Linq_1.Source;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-       
-        }
+        #region Question01
+        ////1. Get all products from the "Seafood" category. Print each product's name and price
+        //var Seafood = ProductList.Where(P=>P.Category == "Seafood")
+        //                            .Select(P=> new {  P.ProductName , P.UnitPrice} );
+
+
+        //foreach (var i in Seafood)
+        //{
+        //    Console.WriteLine(i);
+
+        //}
+
+
+
+        //Query Syntax
+
+        //Seafood = from p in ProductList
+        //          where p.Category == "Seafood"
+        //          select new { p.ProductName, p.UnitPrice };
+
+
+
+
+        #endregion
     }
 }
